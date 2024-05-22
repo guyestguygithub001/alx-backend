09da874377f4acfc096cd7449ead2be7cb970400
guyestguygithub001 <danielene050@gmail.com>
2024-05-22 19:40:26 +0100

    LL

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,35 +1,48 @@
 #!/usr/bin/env python3
+"""
+This module contains the FIFOCache class which inherits from BaseCaching
+and is a caching system following the First In, First Out (FIFO) algorithm.
+"""
 
-# Task 1: Implementation of a FIFO (First In, First Out) caching system
-
-from collections import OrderedDict
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
-    # FIFOCache is a class that inherits from BaseCaching and implements a FIFO caching system
+    """
+    A FIFOCache class that inherits from BaseCaching and is a caching system.
+    This caching system follows the First In, First Out (FIFO) algorithm.
+    """
 
     def __init__(self):
+        """
+        Initialize the class.
+        """
         super().__init__()
-        # Initialize the cache_data dictionary as an ordered dictionary to maintain the order of insertion
-        self.cache_data = OrderedDict()
+        self.keys = []
 
     def put(self, key, item):
-        # The put method stores the item with a given key in the cache_data dictionary
-        # It only stores the item if both the key and item are not None
-        # If the cache is full (i.e., has more items than MAX_ITEMS), it removes the oldest item before storing the new one
-
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
-        self.cache_data[key] = item
+        """
+        Assign to the dictionary self.cache_data the item value for the key key.
+        If key or item is None, this method should not do anything.
+        If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
+        you must discard the first item put in cache (FIFO algorithm)
+        you must print DISCARD: with the key discarded and following by a new line
+        """
+        if key is not None and item is not None:
+            if key not in self.keys:
+                if len(self.keys) >= self.MAX_ITEMS:
+                    discarded_key = self.keys.pop(0)
+                    del self.cache_data[discarded_key]
+                    print('DISCARD: {}'.format(discarded_key))
+                self.keys.append(key)
+            else:
+                self.keys.remove(key)
+                self.keys.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        # The get method retrieves the value associated with a given key from the cache_data dictionary
-        # If the key is not found in the dictionary, it returns None
-
+        """
+        Return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data, return None.
+        """
         return self.cache_data.get(key, None)
-
