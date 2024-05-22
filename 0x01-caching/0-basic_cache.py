9fd06e4b8c6a5ec5508966394437af484783dc37
guyestguygithub001 <danielene050@gmail.com>
2024-05-22 19:37:00 +0100

    <<>>

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
-
-# Task 0: Implementation of a basic dictionary
+"""
+This module contains the BasicCache class which inherits from BaseCaching
+and is a basic caching system.
+"""
 
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-    # BasicCache is a class that inherits from BaseCaching and implements a basic caching system
+    """
+    A BasicCache class that inherits from BaseCaching and is a caching system.
+    This caching system doesn’t have limit.
+    """
 
     def put(self, key, item):
-        # The put method stores the item with a given key in the cache_data dictionary
-        # It only stores the item if both the key and item are not None
+        """
+        Assign to the dictionary self.cache_data the item value for the key key.
+        If key or item is None, this method should not do anything.
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        # The get method retrieves the value associated with a given key from the cache_data dictionary
-        # If the key is not found in the dictionary, it returns None
-
+        """
+        Return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data, return None.
+        """
         return self.cache_data.get(key, None)
 
